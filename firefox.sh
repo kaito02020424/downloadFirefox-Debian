8afc8622bd426e72fa2527174f63b6a0e07d1d20
kaito02020424 <kaito02020424@gmail.com>
2023-04-21 10:23:32 +0900

    Add:apt install firefox

--- firefox.sh
+++ firefox.sh
@@ -1,1 +1,5 @@
 #! /bin/bash
+sudo apt install -y nano
+sudo echo "deb http://ftp.jp.debian.org/debian sid main" >> /etc/apt/sources.list
+sudo apt update -y && sudo apt upgrade -y
+sudo apt install -y firefox
